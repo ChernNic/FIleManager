e37e67567d3a5539764b2c59deed9aa6c7fe3699
Николай Чернявский <75169213+ChernNic@users.noreply.github.com>
2022-11-13 15:50:49 +0300

    first commit

---
+++ FileManager/Menu.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FileManager
+{
+    internal class Menu
+    {
+        public int SelectedIndex;
+        private string[] Options;
+        private string Label;
+        private string FullOrder;
+
+        public Menu(string label, string[] options, string fullOrder)
+        {
+            Label = label;
+            Options = options;
+            SelectedIndex = 0;
+            FullOrder = fullOrder;
+        }
+        public void Display_Options()
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine(Label);
+            Console.SetCursorPosition(0, 1);
+            
+            for (int i = 0; i < Options.Length; i++)
+            {
+                string SelectedOption = Options[i];
+
+                if (i == SelectedIndex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                Console.WriteLine(SelectedOption);
+            }
+            Console.ResetColor();
+
+        }
+        public int Run()
+        {
+            ConsoleKey Key_Pressed;
+            do
+            {
+                
+                new Thread(Display_Options).Start();
+
+                ConsoleKeyInfo Key_Inf = Console.ReadKey(true);
+                Key_Pressed = Key_Inf.Key;
+
+                if (Key_Pressed == ConsoleKey.UpArrow)
+                {
+                    SelectedIndex--;
+                    if (SelectedIndex < 0)
+                    {
+                        SelectedIndex = Options.Length - 1;
+                    };
+                }
+                else if (Key_Pressed == ConsoleKey.DownArrow)
+                {
+                    SelectedIndex++;
+                    if (SelectedIndex >= Options.Length)
+                    {
+                        SelectedIndex = 0;
+                    };
+                }
+            } while (Key_Pressed != ConsoleKey.Enter);
+
+
+
+            return SelectedIndex;
+        }
+    }
+}
---
+++ FileManager/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FileManager
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int SelectedIndex;
+
+            string[] dirs = Directory.GetDirectories(@"C:\");
+
+            ConsoleKeyInfo PressedKey;
+            do
+            {
+                Menu menu = new Menu("afafaf", dirs, "dadadadd");
+                SelectedIndex = menu.Run();
+                PressedKey = Console.ReadKey();
+                dirs = Directory.GetDirectories(dirs[SelectedIndex]);
+            } while(PressedKey.Key != ConsoleKey.Escape);
+
+
+        }
+    }
+}
