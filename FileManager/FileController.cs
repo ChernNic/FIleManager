a83af1cd2cd773c6b55d8a65dde78cff1fd95adc
Николай Чернявский <75169213+ChernNic@users.noreply.github.com>
2022-11-17 01:52:15 +0300

    afafaf

--- FileManager/FileController.cs
+++ FileManager/FileController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace FileManager
 {
@@ -11,66 +12,186 @@ namespace FileManager
     {
         public static string[] GetDirectoryInfo(string path)
         {
+            string[] result;
+
             string[] dirs = Directory.GetDirectories(path);
 
-            string[] result = new string[dirs.Length];
-
-            DateTime[] dirsCreationTime = new DateTime[dirs.Length];
-            for (int i = 0; i < dirs.Length; i++)
-            {
-                dirsCreationTime[i] = Directory.GetCreationTime(dirs[i]);
-            }
-
             for (int i = 0; i < dirs.Length; i++)
             {
                 string gap = "                                                      ";
-                result[i] = dirs[i].Substring(dirs[i].LastIndexOf(@"\") + 1) + gap.Substring(dirs[i].Substring(dirs[i].LastIndexOf(@"\") + 1).Length) + dirsCreationTime[i].ToString("f");
-            }
-            return result;
-        }
+                dirs[i] = dirs[i].Substring(dirs[i].LastIndexOf(@"\") + 1) + gap.Substring(dirs[i].Substring(dirs[i].LastIndexOf(@"\") + 1).Length) + Directory.GetCreationTime(dirs[i]).ToString("f");
 
-        public static string[] GetFileInfo(string path)
-        {
+            }
+
             string[] files = Directory.GetFiles(path);
 
-            string[] result = new string[files.Length];
-
-            DateTime[] filesCreationTime = new DateTime[files.Length];
-            for (int i = 0; i < files.Length; i++)
-            {
-                filesCreationTime[i] = File.GetCreationTime(files[i]);
-            }
-
             for (int i = 0; i < files.Length; i++)
             {
                 string gap = "                                                      ";
-                result[i] = files[i].Substring(files[i].LastIndexOf(@"\") + 1) + gap.Substring(files[i].Substring(files[i].LastIndexOf(@"\") + 1).Length) + filesCreationTime[i].ToString("f");
+                try
+                {
+                    files[i] = files[i].Substring(files[i].LastIndexOf(@"\") + 1) + gap.Substring(files[i].Substring(files[i].LastIndexOf(@"\") + 1).Length) + File.GetCreationTime(files[i]).ToString("f");
+                }
+                catch (System.ArgumentOutOfRangeException)
+                {
+                    files[i] = files[i].Substring(files[i].LastIndexOf(@"\") + 1);
+                }
+                
             }
-            return result;
+
+            return result = dirs.Concat(files).ToArray();
         }
-        
+
         public static string[] GetPaths(string path)
         {
+            string[] result;
             string[] dirs = Directory.GetDirectories(path);
             string[] files = Directory.GetFiles(path);
-            string[] result = dirs.Concat(files).ToArray();
-            return result;
+
+            return result = dirs.Concat(files).ToArray();
         }
 
-        public static void OpenFile(string path)
-        {
+        public static void DeleteFile(string path)
+        { 
             try
             {
-                File.Open(path, FileMode.Open);
+                Directory.Delete(path,true);
             }
-            catch (System.IO.IOException)
+            catch (Exception)
+            {
+                File.Delete(path);                
+            }
+        }
+
+        private static int SelectedIndex;
+        private static DriveInfo[] AllDrives = DriveInfo.GetDrives();
+
+        public static string DriveMenu()
+        {
+            Console.Clear();
+            int selectedIndex = Run();
+            return AllDrives[selectedIndex].Name;
+        }
+
+        private static void GetDrivesInfo()
+        {
+            string[] result = new string[AllDrives.Length];
+            int j = 0;
+            for (int i = 0; i < AllDrives.Length; i++)
             {
 
-            }
-            finally
-            {
+                if (AllDrives[i].IsReady == true)
+                {
+                    string drive = ($"\n{AllDrives[i].VolumeLabel}({AllDrives[i].Name})\nFile System: {AllDrives[i].DriveFormat}\n{AllDrives[i].TotalFreeSpace / 1073741824} GB free in {AllDrives[i].TotalSize / 1073741824} GB");
 
+                    double condition = 0;
+                    condition = Convert.ToDouble(AllDrives[i].TotalFreeSpace) / Convert.ToDouble(AllDrives[i].TotalSize);
+                    condition = (Math.Round(condition, 1));
+
+                    switch (condition)
+                    {
+                        case 0:
+                            {
+                                drive += "\n████████████████████████████████████████";
+                                break;
+                            }
+                        case 0.1:
+                            {
+                                drive += "\n████████████████████████████████████░░░░";
+                                break;
+                            }
+                        case 0.2:
+                            {
+                                drive += "\n████████████████████████████████░░░░░░░░";
+                                break;
+                            }
+                        case 0.3:
+                            {
+                                drive += "\n████████████████████████████░░░░░░░░░░░░";
+                                break;
+                            }
+                        case 0.4:
+                            {
+                                drive += "\n████████████████████████░░░░░░░░░░░░░░░░";
+                                break;
+                            }
+                        case 0.5:
+                            {
+                                drive += "\n████████████████████░░░░░░░░░░░░░░░░░░░░";
+                                break;
+                            }
+                        case 0.6:
+                            {
+                                drive += "\n████████████████░░░░░░░░░░░░░░░░░░░░░░░░ ";
+                                break;
+                            }
+                        case 0.7:
+                            {
+                                drive += "\n████████████░░░░░░░░░░░░░░░░░░░░░░░░░░░░";
+                                break;
+                            }
+                        case 0.8:
+                            {
+                                drive += "\n████████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░";
+                                break;
+                            }
+                        case 0.9:
+                            {
+                                drive += "\n████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░";
+                                break;
+                            }
+                        case 1:
+                            {
+                                drive += "\n░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░";
+                                break;
+                            }
+                    }
+                    drive += "\n----------------------------------------------";
+
+                    if (i == SelectedIndex)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    Console.SetCursorPosition(0, j);
+                    j += 5;
+                    Console.Write($"{drive}");
+                }
             }
+        }
+
+        private static int Run()
+        {
+            ConsoleKey keyPressed;
+            do
+            {
+                new Thread(GetDrivesInfo).Start();
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                keyPressed = keyInfo.Key;
+
+                if (keyPressed == ConsoleKey.UpArrow)
+                {
+                    SelectedIndex--;
+                    if (SelectedIndex < 1)
+                    {
+                        SelectedIndex = AllDrives.Length - 1;
+                    }
+                }
+                else if (keyPressed == ConsoleKey.DownArrow)
+                {
+                    SelectedIndex++;
+                    if (SelectedIndex > AllDrives.Length)
+                    {
+                        SelectedIndex = 0;
+                    }
+                }
+            } while (keyPressed != ConsoleKey.Enter);
+            Console.Clear();
+            return SelectedIndex;
         }
     }
 }
