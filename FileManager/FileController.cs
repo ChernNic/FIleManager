43df4e9c39c12940d9cc00dadb813d924791d62d
Николай Чернявский <75169213+ChernNic@users.noreply.github.com>
2022-11-16 11:25:01 +0300

    e

--- FileManager/FileController.cs
+++ FileManager/FileController.cs
@@ -31,20 +31,20 @@ namespace FileManager
 
         public static string[] GetFileInfo(string path)
         {
-            string[] dirs = Directory.GetDirectories(path);
+            string[] files = Directory.GetFiles(path);
 
-            string[] result = new string[dirs.Length];
+            string[] result = new string[files.Length];
 
-            DateTime[] dirsCreationTime = new DateTime[dirs.Length];
-            for (int i = 0; i < dirs.Length; i++)
+            DateTime[] filesCreationTime = new DateTime[files.Length];
+            for (int i = 0; i < files.Length; i++)
             {
-                dirsCreationTime[i] = Directory.GetCreationTime(dirs[i]);
+                filesCreationTime[i] = File.GetCreationTime(files[i]);
             }
 
-            for (int i = 0; i < dirs.Length; i++)
+            for (int i = 0; i < files.Length; i++)
             {
                 string gap = "                                                      ";
-                result[i] = dirs[i].Substring(dirs[i].LastIndexOf(@"\") + 1) + gap.Substring(dirs[i].Substring(dirs[i].LastIndexOf(@"\") + 1).Length) + dirsCreationTime[i].ToString("f");
+                result[i] = files[i].Substring(files[i].LastIndexOf(@"\") + 1) + gap.Substring(files[i].Substring(files[i].LastIndexOf(@"\") + 1).Length) + filesCreationTime[i].ToString("f");
             }
             return result;
         }
