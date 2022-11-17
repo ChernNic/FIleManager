e63c853e598361c5ea4385fdf0ca6b4093720d7a
Николай Чернявский <75169213+ChernNic@users.noreply.github.com>
2022-11-17 20:20:07 +0300

    final

--- FileManager/Program.cs
+++ FileManager/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
+﻿
 
 namespace FileManager
 {
@@ -11,21 +7,9 @@ namespace FileManager
         
         static void Main(string[] args)
         {
-            //string path = @"C:\";
-
-            //string[] Options = Directory.GetFileSystemEntries(path);
-
-            //for (int i = 0; i < Options.Length; i++)
-            //{
-            //    Console.WriteLine(Options[i]);
-            //}|—
-
-            //Console.ReadLine();
-
             Menu menu = new Menu();
             menu.MainMenu();
         }
-        
     }
 }
 
